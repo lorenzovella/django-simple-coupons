6c98571916d7282c65c378de6e3c4ff693b68125
Lorenzo V <lorenzo.vella@live.com>
2020-09-09 15:36:47 -0300

    Update helpers.py

--- django_simple_coupons/helpers.py
+++ django_simple_coupons/helpers.py
@@ -2,14 +2,14 @@ import string
 import random
 
 from django.conf import settings
-
+from clientflow.app.models import Cliente
 
 def get_coupon_code_length(length=12):
     return settings.DSC_COUPON_CODE_LENGTH if hasattr(settings, 'DSC_COUPON_CODE_LENGTH') else length
 
 
 def get_user_model():
-    return app.models.Cliente
+    return Cliente
 
 
 def get_random_code(length=12):
